34b7315fc1fc91de5cd6a0ae9e8b7dc41abf46da
shvrifkaaa <78095051+shvrifkaaa@users.noreply.github.com>
2023-01-08 21:24:01 +0600

    added HW for lesson 2

---
+++ month2-lessons-HW/lesson2/app.js
@@ -0,0 +1,21 @@
+// var numbers = [1,2,3,4,5]
+
+// var childrenYears = [2002, 2005, 2007, 1999, 2013, 2015]
+// var realChilDrenYears = []
+
+// var currentYear = 2022;
+
+// for (var childrenYear of childrenYears) {
+//     var ChildAge = currentYear - childrenYear
+//     if (currentYear - childrenYear <= 12) {
+//         realChilDrenYears.push(ChildAge)
+//     }
+// }
+
+// console.log(realChilDrenYears);
+
+// for(i = 0; i < childrenYears.length; i+= 2){
+//     console.log(childrenYears[i]);
+// }
+
+                                                                                     
